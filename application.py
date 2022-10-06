d55cae704a923282603bc9b337bc91f429392335
xueningleng <sherryleng27@gmail.com>
2022-10-06 18:46:27 -0400

    application v1.0

---
+++ application.py
@@ -0,0 +1,67 @@
+import logging.handlers
+from flask import Flask, Response, request
+from datetime import datetime
+import json
+from registration import Registration
+from flask_cors import CORS
+
+application = Flask(__name__)
+CORS(application)
+
+
+# some bits of text for the page.
+header_text = '''
+    <html>\n<head> <title>Registration</title> </head>\n<body> <h2>REGISTRATION PAGE</h2> '''
+instructions = '''
+    <p><em>Hint</em>: <br>
+    Append '/api/health' to the URL to test connectivity<br>
+    Append '/api/register/?phone=value&first_name=value&last_name=value&password=value' to the URL with your own value to register!
+    </p>\n'''
+home_link = '<p><a href="/">Back</a></p>\n'
+footer_text = '</body>\n</html>'
+
+# add a rule for the index page.
+application.add_url_rule('/', 'index', (lambda: header_text +
+instructions + footer_text))
+
+@application.get("/api/health")
+def get_health():
+    t = str(datetime.now())
+    msg = {
+        "name": "thumbsup-registration",
+        "health": "Good",
+        "at time": t
+    }
+
+    result = Response(json.dumps(msg), status=200, content_type="application/json")
+
+    return result
+
+@application.route("/api/register/", methods = ['GET', 'POST'])
+def register():
+    # ?phone=value&first_name=value&last_name=value&password=value
+    # test: {ENDPOINT}/api/register/?phone=2223330000&first_name=foo&last_name=bar&password=88888888
+    phone = request.args.get('phone', type=str)
+    fname = request.args.get('first_name', type=str)
+    lname = request.args.get('last_name', type=str)
+    pword = request.args.get('password', type=str)
+    msg = {
+        "phone" : phone,
+        "name" : fname+" "+lname,
+        "pword" : "*"*len(pword),
+    }
+
+    err = Registration.add_user(phone, fname, lname, pword);
+    if not err:
+        msg["result"]= "Registration succeeded!"
+        result = Response(json.dumps(msg), status=200, content_type="application/json")
+
+    else:
+        result = Response(err.args[1], status=404, content_type="text/plain")
+
+
+    return result
+
+if __name__ == '__main__':
+    application.debug = True
+    application.run(host="0.0.0.0", port=5011)
---
+++ registration.py
@@ -0,0 +1,60 @@
+import os
+import pymysql
+
+
+class Registration:
+
+    def __int__(self):
+        pass
+
+    @staticmethod
+    def _get_connection():
+        usr = os.environ.get("RDS_USERNAME")
+        pw = os.environ.get("RDS_PASSWORD")
+        h = os.environ.get("RDS_HOSTNAME")
+        db = os.environ.get("RDS_DB_NAME")
+        port = os.environ.get("RDS_PORT")
+        conn = pymysql.connect(
+            user=usr,
+            password=pw,
+            host=h,
+            database = db,
+            cursorclass=pymysql.cursors.DictCursor,
+            autocommit=True
+        )
+        return conn
+
+    @staticmethod
+    def create_user_table():
+        sql = ("CREATE TABLE IF NOT EXISTS `registration` ("
+        "  `phone` varchar(10) NOT NULL ,"
+        "  `first_name` varchar(20) NOT NULL,"
+        "  `last_name` varchar(20) NOT NULL,"
+        "  `password` varchar(20) NOT NULL,"
+        "  PRIMARY KEY (`phone`)"
+        ") ENGINE=InnoDB")
+
+        conn = Registration._get_connection()
+        cur = conn.cursor()
+        cur.execute(sql)
+        result = cur.fetchone()
+        conn.commit()
+        conn.close()
+        return result
+
+    @staticmethod
+    def add_user(phone, fname, lname, pword):
+        print(phone)
+        sql = "INSERT INTO `registration` (phone, first_name, last_name, password) VALUES (%s, %s, %s, %s)"
+        conn = Registration._get_connection()
+        cur = conn.cursor()
+        val = (phone, fname, lname, pword)
+        try:
+            cur.execute(sql, val)
+        except(pymysql.err.IntegrityError) as e:
+
+            return e
+        finally:
+            conn.commit()
+            conn.close()
+        return None
